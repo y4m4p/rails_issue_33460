30b6ecf09add13feb3141f4b942671fa2db905c4
y4m4p <m-yamashita@tsukuruba.com>
2018-07-30 15:10:07 +0900

    add broken helper_method to WebPagesHelper

--- app/helpers/web_pages_helper.rb
+++ app/helpers/web_pages_helper.rb
@@ -1,2 +1,9 @@
 module WebPagesHelper
+  def helper_method
+    if true
+      link_to 'somewhere', some_view_path(foo: 'true')
+    else
+      #link_to 'somewhere', some_view_path(foo: 'false')
+    end
+  end
 end
