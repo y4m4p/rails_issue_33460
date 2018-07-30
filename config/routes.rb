a1ce57b8371f333776fdb6659ec380ee0052fd2a
y4m4p <m-yamashita@tsukuruba.com>
2018-07-30 16:24:55 +0900

    add routes to localhost:3000/some_view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  get 'some_view' => 'web_pages#some_view'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
