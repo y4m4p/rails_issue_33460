d4d4397779049557e9ef88668c5b46b43ef30e4f
y4m4p <m-yamashita@tsukuruba.com>
2018-07-31 20:19:33 +0900

    add more specific details about the helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def broken_helper_in_application_helper
+    ## Uncommenting the process below will crash the rails server whenever you render any view.
+    ## This occurs EVEN IF YOU DO NOT USE THIS METHOD IN ANY VIEW.
+    ## Accessing any page in your server will be impossible by uncommenting this.
+    #
+    #if true
+    #  link_to 'somewhere', some_view_path(foo: 'true')
+    #else
+    #  link_to 'somewhere', some_view_path(foo: 'false')
+    #end
+  end
 end
--- app/helpers/web_pages_helper.rb
+++ app/helpers/web_pages_helper.rb
@@ -1,9 +1,45 @@
 module WebPagesHelper
-  def helper_method
+  # This method will crash the server with the following error.
+  # ...vendor/bundle/ruby/2.5.0/gems/bootsnap-1.3.1/lib/bootsnap/compile_cache/iseq.rb:12: [BUG] Segmentation fault at 0x0000000000000000
+  def broken_helper_method
+    if true
+      link_to 'somewhere', some_view_path(foo: 'true')
+    else
+      link_to 'somewhere', some_view_path(foo: 'false')
+    end
+
+    ## OR you can write it like this
+    # if false
+    #   link_to 'somewhere', some_view_path(foo: 'true')
+    # else
+    #   link_to 'somewhere', some_view_path(foo: 'false')
+    # end
+    #
+    # Either way, if you write the if condition using true/false and writing a
+    # `link_to` method using a url_helper with a request parameter in the never processed
+  end
+
+  def safe_helper_method
     if true
       link_to 'somewhere', some_view_path(foo: 'true')
     else
       #link_to 'somewhere', some_view_path(foo: 'false')
     end
+
+    ## OR you can write it like this
+    # if false
+    #   #link_to 'somewhere', some_view_path(foo: 'true')
+    # else
+    #   link_to 'somewhere', some_view_path(foo: 'false')
+    # end
+    #
+    #
+    ## If you do not specify the request parameter for the unused else statement, it is also SAFE
+    # if true
+    #   link_to 'somewhere', some_view_path(foo: 'true')
+    # else
+    #   link_to 'somewhere', some_view_path
+    # end
+    #
   end
 end
